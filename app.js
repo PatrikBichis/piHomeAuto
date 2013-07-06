034426ea36129302b0fdadfa94e1380ccf941edb
Michael Sund <michael@osund.com>
2013-07-06 14:30:57 +0200

    cleaning

--- app.js
+++ app.js
@@ -43,9 +43,6 @@ app.get('/net', function(req, res){
 app.get('/map', function(req, res){
   res.render('map', { title: 'piHomeAuto', subTitle: 'Karta' });
 })
-app.get('/map_edit', function(req, res){
-  res.render('map_edit', { title: 'piHomeAuto', subTitle: 'Editera karta' });
-})
 
 app.get('/units', function(req, res){
   res.render('units', { title: 'piHomeAuto', subTitle: 'Enheter' });
